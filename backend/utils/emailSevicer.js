9b4803c58229e54bc4ea1ad9dc0020d1cf74a3f9
dvduy25 <babooan01@gmail.com>
2026-03-08 20:56:35 +0700

    update

--- backend/utils/emailSevicer.js
+++ backend/utils/emailSevicer.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
