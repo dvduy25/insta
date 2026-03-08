778ae9f6d0e1acb8ad736302458c02069e48b365
dvduy25 <babooan01@gmail.com>
2026-03-08 20:45:08 +0700

    update

--- backend/utils/emailSevicer.js
+++ backend/utils/emailSevicer.js
@@ -1,24 +1,22 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
 const sendEmail = async (to, subject, html) => {
-  const mailOptions = {
-    from: `"InstaClone" <${process.env.EMAIL_USER}>`,
-    to: to,
-    subject: subject,
-    html: html
-  };
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-  await transporter.sendMail(mailOptions);
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
+  };
+
+ await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
