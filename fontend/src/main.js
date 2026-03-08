dc7eb7c6478dd9c00b24315dbf9f7028d40d26b4
dvduy25 <babooan01@gmail.com>
2026-03-08 18:15:19 +0700

    update

--- fontend/src/main.js
+++ fontend/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index'
-axios.defaults.baseURL = 'https://insta-123.onrender.com';
-axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = 'https://insta-123.onrender.com';
+// axios.defaults.withCredentials = true;
 createApp(App).use(router).mount('#app')
