04491ebc52f816158598ee3c74fdaa7a684e757e
dvduy25 <babooan01@gmail.com>
2026-03-08 18:19:10 +0700

    update

--- fontend/src/App.vue
+++ fontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <router-view v-slot="{ Component }">
     <keep-alive include="Home,Message,Explore">
-      <component :is="Component" v-if="['Home'].includes(Component.name)" />
+      <component :is="Component" v-if="['Home'].includes(Component?.name)" />
     </keep-alive>
 
-    <component :is="Component" v-if="!['Home'].includes(Component.name)" />
+    <component :is="Component" v-if="!['Home'].includes(Component?.name)" />
   </router-view>
 </template>
 
